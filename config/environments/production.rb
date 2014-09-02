55d0abe98b27dadc84f07be994123212091d61e1
Benedikt Böhm <bb@xnull.de>
2014-09-02 10:00:53 +0200

    Revert "use STDOUT logger"
    This reverts commit b18b74ebf30789b8ff540f9b9615f223713c9070.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,9 +42,14 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
-  # Use a different logger for distributed setups
-  config.logger = Logger.new(STDOUT)
-  config.lograge.enabled = true
+  # Set to :debug to see everything in the log.
+  config.log_level = :info
+
+  # Prepend all log lines with the following tags.
+  # config.log_tags = [ :subdomain, :uuid ]
+
+  # Use a different logger for distributed setups.
+  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
@@ -66,4 +71,10 @@ Rails.application.configure do
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
+
+  # Disable automatic flushing of the log to improve performance.
+  # config.autoflush_log = false
+
+  # Use default logging formatter so that PID and timestamp are not suppressed.
+  config.log_formatter = ::Logger::Formatter.new
 end
