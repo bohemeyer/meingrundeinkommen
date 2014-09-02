9ed7cbc0faa30e2d9f5f85e12fcb8dae44d9eef7
Benedikt Böhm <bb@xnull.de>
2014-09-02 14:38:15 +0200

    use mailjet in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,7 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+  config.action_mailer.delivery_method = :mailjet
   config.action_mailer.default_url_options = {
     host: 'https://www.mein-grundeinkommen.de'
   }
