c7594341de87397e559b0c15c8550626ab9cfc1d
Michael Bohmeyer <michael.bohmeyer@admineo.de>
2014-11-26 01:39:30 +0100

    mail test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,6 +15,8 @@ Rails.application.configure do
     host: 'https://www.mein-grundeinkommen.de'
   }
 
+  config.action_mailer.default_options = { from: "\"Mein Grundeinkommen\" <micha@meinbge.de>" }
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address   => "smtp.mandrillapp.com",
