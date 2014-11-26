2681ce024dd595613670bfc0e3cceb9c7dea1690
Michael Bohmeyer <michael.bohmeyer@admineo.de>
2014-11-26 01:32:20 +0100

    mail change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,10 +20,12 @@ Rails.application.configure do
     :address   => "smtp.mandrillapp.com",
     :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
     :enable_starttls_auto => true, # detects and uses STARTTLS
-    :user_name => "admin@mein-grundeinkommen.de",
+    :user_name => "micha@meinbge.de",
     :password  => "W_8cahHMZNbr66JjX_-zYw", # SMTP password is any valid API key
     :authentication => 'login', # Mandrill supports 'plain' or 'login'
     :domain => 'meinbge.de', # your domain to identify your server when connecting
+    :from => "\"Mein Grundeinkommen\" <micha@meinbge.de>"
+
   }
 
   # Full error reports are disabled and caching is turned on.
