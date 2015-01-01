1861e5f732482150a5bea30d56e10b7861dd5efe
Michael Bohmeyer <michael.bohmeyer@admineo.de>
2015-01-01 23:33:37 +0000

    nerv

--- app/controllers/api/winners_controller.rb
+++ app/controllers/api/winners_controller.rb
@@ -3,7 +3,8 @@ class Api::WinnersController < ApplicationController
   #everyone's a winner baby, that's for sure! :)
 
   def index
-    render json: User.select(:id,:name,:avatar).where('winner > 0').order('winner asc')
+    render json: User.select('id,name,avatar').where('winner > 0').order('winner asc')
+  	#render json: User.find(:all,:select => 'id,name,avatar', :conditions => 'winner > 0', :order =>  'winner asc')
   end
 
 end
