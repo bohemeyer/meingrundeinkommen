d03e182dd38c8c54365aa427984f8c9f0a221282
Michael Bohmeyer <michael.bohmeyer@admineo.de>
2015-02-02 23:29:02 +0100

    added missing queries

--- app/controllers/api/statistics_controller.rb
+++ app/controllers/api/statistics_controller.rb
@@ -13,40 +13,58 @@ class Api::StatisticsController < ApplicationController
       :participantsUnconfirmedButOrderedCrowdcardButNotYetSent => "user_id not in (select user_id from flags where name like 'CrowdcardNumber') and user_id not in (select user_id from flags where name like 'hasHadCrowdbar') and user_id not in (select user_id from flags where name like 'crowdAppVisits') and user_id in (select user_id from crowdcards where sent is null)"
     }
 
+    queries_not = {
+      :notParticipating => "",
+      :notParticipatingButCrowdToolUser => " and (id in (select user_id from flags where name like 'CrowdcardNumber') or id in (select user_id from flags where name like 'hasHadCrowdbar') or id in (select user_id from flags where name like 'crowdAppVisits'))"
+    }
+
 
     respond_to do |format|
       format.json {
 
         newsletter_only = ''
-        base = "select users.email, REPLACE(users.name,',','') from users, chances where users.id = chances.user_id #{newsletter_only} and is_child = 0 and "
+        base     = "select users.email, REPLACE(users.name,',','') from users, chances where users.id = chances.user_id #{newsletter_only} and is_child = 0 and "
+        base_not = "select users.email, REPLACE(users.name,',','') from users where users.id not in (select user_id from chances) #{newsletter_only} "
 
         stats = {}
         queries.each do |key,query|
           stats[key] = ActiveRecord::Base.connection.execute("#{base} #{query}").count
         end
 
+        queries_not.each do |key,query|
+          stats[key] = ActiveRecord::Base.connection.execute("#{base_not} #{query}").count
+        end
+
         stats['crowdcardOrders'] = Crowdcard.all.count
         stats['crowdcardsOrdered'] = Crowdcard.sum(:number_of_cards)
         stats['participantsWithChildren'] = Chance.all.count
         stats['participants'] = Chance.where(:is_child => 0).count
+        stats['activeAccounts'] = User.where('confirmed_at is not null').count
+        stats['newsletterSubscriptions'] = User.where('confirmed_at is not null and newsletter = 1').count
 
         render json: stats
       }
       format.csv {
         if current_user && current_user.admin? && params[:stat]
           newsletter_only = " and confirmed_at is not null and newsletter = 1 "
-          base = "select users.email, REPLACE(users.name,',','') from users, chances where users.id = chances.user_id #{newsletter_only} and is_child = 0 and "
+          base     = "select users.email, REPLACE(users.name,',','') from users, chances where users.id = chances.user_id #{newsletter_only} and is_child = 0 and "
+          base_not = "select users.email, REPLACE(users.name,',','') from users where id not in (select user_id from chances) #{newsletter_only} "
 
+          if queries[params[:stat].to_sym]
+            q = queries[params[:stat].to_sym]
+            b = base
+          else
+            q = queries_not[params[:stat].to_sym]
+            b = base_not
+          end
 
-            r = CSV.generate() do |csv|
-              ActiveRecord::Base.connection.execute("#{base} #{queries[params[:stat].to_sym]}").each do |row|
-                csv << row
-              end
+          r = CSV.generate() do |csv|
+            ActiveRecord::Base.connection.execute("#{b} #{q}").each do |row|
+              csv << row
             end
+          end
 
-            send_data r
-
-
+          send_data r
 
         end
       }
