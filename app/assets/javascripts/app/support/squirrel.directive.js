f0a57c814755e5c493487d89a97e6bb06df3f67f
Michael Bohmeyer <michael.bohmeyer@admineo.de>
2015-03-16 11:21:02 +0100

    a/b test with amount

--- app/assets/javascripts/app/support/squirrel.directive.js
+++ app/assets/javascripts/app/support/squirrel.directive.js
@@ -17,8 +17,8 @@
                 $scope.user = Security
 
                 var vm = this;
-                vm.price = 5;
-                vm.priceSociety = 5;
+                vm.price = 10;
+                vm.priceSociety = 10;
                 vm.priceBge = 0;
                 vm.auth = Security.isAuthenticated();
 
