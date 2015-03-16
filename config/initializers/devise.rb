029aae7e595a9ec5cf11b91d71b968578b57a062
Michael Bohmeyer <michael.bohmeyer@admineo.de>
2015-03-16 06:56:07 +0100

    changed devise settings

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -96,7 +96,7 @@ Devise.setup do |config|
   # able to access the website for two days without confirming his account,
   # access will be blocked just in the third day. Default is 0.days, meaning
   # the user cannot access the website without confirming his account.
-  config.allow_unconfirmed_access_for = 7.days
+  config.allow_unconfirmed_access_for = 300.days
 
   # A period that the user is allowed to confirm their account before their
   # token becomes invalid. For example, if set to 3.days, the user can confirm
@@ -104,7 +104,7 @@ Devise.setup do |config|
   # their account can't be confirmed with the token any more.
   # Default is nil, meaning there is no restriction on how long a user can take
   # before confirming their account.
-  config.confirm_within = 21.days
+  config.confirm_within = nil
 
   # If true, requires any email changes to be confirmed (exactly the same way as
   # initial account confirmation) to be applied. Requires additional unconfirmed_email
