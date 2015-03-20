28e5ca996a53679832ab6fff13baf5fa62d218b5
Michael Bohmeyer <michael.bohmeyer@admineo.de>
2015-03-20 11:38:32 +0100

    added caching for supports

--- app/controllers/api/supports_controller.rb
+++ app/controllers/api/supports_controller.rb
@@ -1,5 +1,8 @@
 class Api::SupportsController < ApplicationController
 
+  caches_page index
+
+
   def create
     support = Support.create(params.permit(:amount_total, :amount_for_income, :amount_internal, :payment_method, :recurring))
 
@@ -27,15 +30,15 @@ class Api::SupportsController < ApplicationController
   end
 
   def index
-    if current_user && current_user.admin? and params[:admin]
-      render json: Support.where('payment_method = "bank" or payment_method like "paypal%"').order(:id => :desc)
-    else
-      if params[:crowdbar]
-        render json: Support.where("comment is not null and payment_method = 'crowdbar'").limit(20)
-      else
+    #if current_user && current_user.admin? and params[:admin]
+    #  render json: Support.where('payment_method = "bank" or payment_method like "paypal%"').order(:id => :desc)
+    #else
+      #if params[:crowdbar]
+      #  render json: Support.where("comment is not null and payment_method = 'crowdbar'").limit(20)
+      #else
         render json: Support.where('comment is not null and payment_completed is not null').limit(30).order(:updated_at => :desc)
-      end
-    end
+      #end
+    #end
   end
 
   def show
