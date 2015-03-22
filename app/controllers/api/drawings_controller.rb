6bff599e5b553a0fdddd7564a7b06d3510b696c4
Michael Bohmeyer <michael.bohmeyer@admineo.de>
2015-03-22 13:10:57 +0100

    changed texts for current drawing

--- app/controllers/api/drawings_controller.rb
+++ app/controllers/api/drawings_controller.rb
@@ -16,29 +16,29 @@ require 'json'
 
 
         drawing[:digets].each_with_index do |d,i|
-          digits << d[:value].to_i if d[:value] && i < 3 && d[:value] != 0 && d[:value] != ""
+          digits << d[:value] if d[:value] && d[:value] != ""
         end
 
 
 
-        if digits.count == 3
-          t = '1-12'  if digits[2] <= 12
-          t = '13-24' if digits[2] >= 13 && digits[2] <= 24
-          t = '25-36' if digits[2] >= 25 && digits[2] <= 36
-          t = '37-48' if digits[2] >= 37 && digits[2] <= 48
-          t = '49-60' if digits[2] >= 49 && digits[2] <= 60
-          digits[2] = t
-        end
+        # if digits.count == 3
+        #   t = '1-12'  if digits[2] <= 12
+        #   t = '13-24' if digits[2] >= 13 && digits[2] <= 24
+        #   t = '25-36' if digits[2] >= 25 && digits[2] <= 36
+        #   t = '37-48' if digits[2] >= 37 && digits[2] <= 48
+        #   t = '49-60' if digits[2] >= 49 && digits[2] <= 60
+        #   digits[2] = t
+        # end
 
 #       letters = ['[1-12]','[13-24]','[25-36]','[37-48]','[49-60]']
 
-        number = digits.join("|")
+        number = digits.join(",")
 
 
 
         if Chance.where("code LIKE ?", "#{number}%").present?
           data[i][:niete] = false
-          if digits.count == 3
+          if digits.count == 6
             data[i][:user] = Chance.where("code = ?", "#{number}").first.user
             data[i][:isChild] = Chance.where("code = ?", "#{number}").first.is_child
             data[i][:childName] = Chance.where("code = ?", "#{number}").first.first_name
