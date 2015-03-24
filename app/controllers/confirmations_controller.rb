d8b5ea57a1e2b5d328d90b9dc92f12c813386107
Michael Bohmeyer <michael.bohmeyer@admineo.de>
2015-03-24 17:08:40 +0100

    "fixed" confirmation token bug

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -5,10 +5,11 @@ class ConfirmationsController < Devise::ConfirmationsController
   def show
 	self.resource = resource_class.confirm_by_token(params[:confirmation_token])
 	yield resource if block_given?
+
 	if resource.errors.empty?
-	respond_with_navigational(resource){ redirect_to after_confirmation_path_for(resource_name, resource) }
+		respond_with_navigational(resource){ redirect_to after_confirmation_path_for(resource_name, resource) }
 	else
-	respond_with_navigational(resource){ redirect_to current_user ? "/boarding?trigger=confirmed" : "/login?confirmation_error=true" }
+		respond_with_navigational(resource){ redirect_to current_user ? "/boarding?trigger=confirmed" : resource.email && !resource.email.empty? ? "/login?email=#{resource.email}&confirmation_error=true" : "/login?confirmation_error=true" }
 	end
   end
 
