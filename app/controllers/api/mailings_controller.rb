055e456e9412059393aa999a051f0942698642c1
Michael Bohmeyer <michael.bohmeyer@admineo.de>
2015-04-28 21:57:27 +0200

    admins only

--- app/controllers/api/mailings_controller.rb
+++ app/controllers/api/mailings_controller.rb
@@ -3,34 +3,36 @@ class Api::MailingsController < ApplicationController
 
   def create
 
-  	possible_user_groups = %w(byids# last_squirrel_id# frst_notification_not_sent is_squirrel sign_up_after# without_crowdbar with_crowdbar has_code participating confirmed with_newsletter)
+  	if current_user.admin?
+	  	possible_user_groups = %w(byids# last_squirrel_id# frst_notification_not_sent is_squirrel sign_up_after# without_crowdbar with_crowdbar has_code participating confirmed with_newsletter)
 
-  	if params[:groups]
-	  	groups = []
+	  	if params[:groups]
+		  	groups = []
 
-	  	users = User
-	  	params[:groups].each_with_index do |g,i|
-	  	  if possible_user_groups.include? g
-	  	  	if params[:group_keys][i].empty?
-	  	  		users = users.send(g)
-	  	  	else
-	  	  		params[:group_keys][i] = params[:group_keys][i].split(',') if g == "find#"
-				users = users.send(g.sub!('#',''),params[:group_keys][i])
-			end
-	  	  end
-	  	end
+		  	users = User
+		  	params[:groups].each_with_index do |g,i|
+		  	  if possible_user_groups.include? g
+		  	  	if params[:group_keys][i].empty?
+		  	  		users = users.send(g)
+		  	  	else
+		  	  		params[:group_keys][i] = params[:group_keys][i].split(',') if g == "find#"
+					users = users.send(g.sub!('#',''),params[:group_keys][i])
+				end
+		  	  end
+		  	end
 
-	    # users = User.send(groups.join('.'))
-	end
+		    # users = User.send(groups.join('.'))
+		end
 
 
 
-	if params[:send]
-		users = [User.find(current_user.id)] if params[:test]
-		test = MailingsMailer.transactionmail(users,params[:subject],params[:body]).deliver
-		render json: test
-	else
-		render json: { count: users ? users.count : 0, groups: possible_user_groups }
+		if params[:send]
+			users = [User.find(current_user.id)] if params[:test]
+			test = MailingsMailer.transactionmail(users,params[:subject],params[:body]).deliver
+			render json: test
+		else
+			render json: { count: users ? users.count : 0, groups: possible_user_groups }
+		end
 	end
   end
 end
