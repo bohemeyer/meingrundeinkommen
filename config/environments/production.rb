e9344d17dbfa1204eb3d2445afd564dd273d0001
Michael Bohmeyer <michael.bohmeyer@admineo.de>
2015-05-01 02:23:00 +0200

    compressing js & css in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  config.cache_classes = false
+  config.cache_classes = true
 
 
   config.action_controller.perform_caching = true
@@ -48,8 +48,8 @@ Rails.application.configure do
   #config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
-  #config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.js_compressor = :uglifier
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   #config.assets.compile = false
