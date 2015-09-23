0272cc6f41c490dd4432624bdd114197acb570c4
Michael Bohmeyer <michael.bohmeyer@admineo.de>
2015-09-23 19:26:50 +0200

    still fixing

--- app/controllers/api/winners_controller.rb
+++ app/controllers/api/winners_controller.rb
@@ -4,8 +4,7 @@ class Api::WinnersController < ApplicationController
   #everyone's a winner baby, that's for sure! :)
 
   def index
-  	winners = User.select('id,name,avatar').where('winner > 0').order('winner asc')
-    render json: winners
+    render json: User.where('winner > 0').order('winner asc')
   end
 
 end
