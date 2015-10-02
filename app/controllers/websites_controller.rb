0fb4ad22eccdb638656c3d4d02c5ac5cc111ce6d
Michael Bohmeyer <michael.bohmeyer@admineo.de>
2015-10-02 13:58:35 +0200

    social media text

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -2,9 +2,20 @@ class WebsitesController < ApplicationController
 
 
   def show
-  	b = request.env['HTTP_USER_AGENT']
+
+
+      if request.fullpath == '/tandem'
+        @title = "#bgeMitDir - Die neue Tandemverlosung"
+        @text = "MeinBGE verlost jetzt 2 Grundeinkommen an 2 Menschen, die sich kennen. Bilde Tandems mit all den Menschen, denen du Grundeinkommen gönnst und erhaltet ein Jahr lang je 1.000 € monatlich. Bedingungslos."
+        @img = 'https://www.mein-grundeinkommen.de/tandemgross.gif'
+      end
+
+    b = request.env['HTTP_USER_AGENT']
   	if b.include?('facebookexternalhit') || b.include?('Twitterbot') # || b.include?('Mozilla')
-  		if params[:mitdir]
+
+
+
+      if params[:mitdir]
   			u = User.find(params[:mitdir])
   			@title = "Ich will #bgeMitDir gewinnen"
   			@text = "MeinBGE verlost dieses Mal gleich 2 Grundeinkommen. Sei du mein Tandem! Wenn es gewinnt, erhalten wir beide ein Jahr lang jeweils 1.000 € monatlich. Bedingungslos."
