f78405ee46d440f871391553870a1b2b3e9f57dc
Michael Bohmeyer <michael.bohmeyer@admineo.de>
2015-10-02 17:54:21 +0200

    fixed name bug

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -5,7 +5,7 @@ class InvitationMailer < ActionMailer::Base
     @tandem = tandem
     @inviter = inviter
     @invitee = invitee
-    mail(to: @invitee.email, subject: "#{@invitee.chances.first.first_name} will #bgeMitDir gewinnen")
+    mail(to: @invitee.email, subject: "#{@inviter.chances.first.first_name} will #bgeMitDir gewinnen")
   end
 
   def inform_about_link_confirmation(tandem,invitee,inviter)
