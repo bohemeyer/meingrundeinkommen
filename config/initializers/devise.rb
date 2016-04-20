a7b36c7d374fcd2194aaa74c7dd4bf0c293a3d3e
ZengineAlex <alexander@zengine.org>
2016-04-20 11:03:39 +0200

    change the mailer_sender

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,7 +6,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
 
-  config.mailer_sender = 'admin@mein-grundeinkommen.de'
+  config.mailer_sender = 'support@mein-grundeinkommen.de'
 
   # Configure the class responsible to send e-mails.
   config.mailer = 'Devise::Mailer'
